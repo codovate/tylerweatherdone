0c49d4dd2d5d96fc51b843df8a6fb7682f9aa9f6
Sam Johnson <SamJohnson@akauk.com>
2018-12-06 20:48:57 +0000

    first commit

---
+++ app/utils/helpers.js
@@ -0,0 +1,42 @@
+var daysMap = {
+  "0": "Sunday",
+  "1": "Monday",
+  "2": "Tuesday",
+  "3": "Wednesday",
+  "4": "Thursday",
+  "5": "Friday",
+  "6": "Saturday"
+};
+
+var monthsMap = {
+  "0" : "Jan",
+  "1" : "Feb",
+  "2" : "Mar",
+  "3" : "Apr",
+  "4" : "May",
+  "5" : "June",
+  "6" : "July",
+  "7" : "Aug",
+  "8" : "Sept",
+  "9" : "Oct",
+  "10": "Nov",
+  "11": "Dec"
+};
+
+function convertTemp(kelvin) {
+  return parseInt((kelvin - 273.15) * 1.8 + 32.0, 10);
+}
+
+//convert unix timestamp to ---> day, month date
+//e.g: 1544007600  becomes ---> Wednesday, Dec 5
+function getDate(unixTimestmap) {
+  var date  = new Date(unixTimestmap * 1000);
+  var day   = daysMap[date.getDay()];
+  var month = monthsMap[date.getMonth()] + " " + date.getDate();
+  return day + ", " + month;
+}
+
+module.exports = {
+  convertTemp: convertTemp,
+  getDate    : getDate
+};
